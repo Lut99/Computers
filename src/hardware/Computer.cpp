90131d91a81418e78607f8b3de429b239feafd05
Lut99 <mullertim99@gmail.com>
2019-12-04 18:37:51 +0100

    Added computer.cpp file

---
+++ src/hardware/Computer.cpp
@@ -0,0 +1,9 @@
+/* COMPUTER.cpp
+*    by tHE iNCREDIBLE mACHINE
+*
+*  This file is the overarching file for the Computer. It is where components
+*  are installed and communication between them happens. Currently, there is
+*  only one computer, but there might be more different configurations in the
+*  future.
+**/
+
