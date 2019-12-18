a7e8273b293b7a36cff7db9b675bf634b235d486
Lut99 <mullertim99@gmail.com>
2019-12-18 10:10:40 +0000

    Finished with alpha implementation of StringConverter::toUInt()

--- src/support/StringConverter.cpp
+++ src/support/StringConverter.cpp
@@ -3,40 +3,105 @@
 *
 *  The StringConverter is a namespace that allows conversion from string to
 *  various other datatypes. In contradiction to readily made available
-*  functions, the amount of forgiveness is very low, and it returns different
-*  exceptions based on what went wrong.
+*  functions, the amount of forgiveness is custumizable, but is set to very
+*  strict by default (NoForgiveness).
 **/
+
+#include <limits.h>
+#include <sstream>
 
 #include "StringConverter.h"
 
-unsigned char StringConverter::toUChar(std::string to_parse) {
+unsigned char StringConverter::toUChar(std::string to_parse, StringConverterMode mode = NoForgiveness) {
 
 }
-char StringConverter::toChar(std::string to_parse) {
+char StringConverter::toChar(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
 
-unsigned short StringConverter::toUShort(std::string to_parse) {
+unsigned short StringConverter::toUShort(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
-short StringConverter::toShort(std::string to_parse) {
+short StringConverter::toShort(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
 
-unsigned int StringConverter::toUInt(std::string to_parse) {
-    unsigned int result = 0;
-    // Work from right to left
-    for (int i = to_parse.length() - 1; i >= 0; i--) {
-
+unsigned int StringConverter::toUInt(std::string to_parse, StringConverterMode mode = NoForgiveness) {
+    // Preparse the string to find the actual number, as specified by the mode
+    std::stringstream sstr;
+    int in_stream = 0;
+    bool non_num = false;
+    for (int i = 0; i < to_parse.length(); i++) {
+        char c = to_parse[i];
+        if (c < '0' || c > '9') {
+            if (mode == ParseFirstNumber) {
+                // If there is something in the buffer, we now have the first number and so we quit
+                if (in_stream > 0) {
+                    break;
+                }
+            } else if (mode == ParseLastNumber) {
+                // Set the flag that a break has happened
+                non_num = true;
+            } else if (mode == AllowSpaces) {
+                if (c != ' ') {
+                    throw IllegalCharacterException(c, to_parse, mode);
+                }
+            } else {
+                throw IllegalCharacterException(c, to_parse, mode);
+            }
+        } else {
+            // Clear the buffer if the non_num flag is set
+            if (non_num) {
+                non_num = false;
+                sstr.str("");
+                sstr.clear();
+                in_stream = 0;
+            }
+            sstr << c;
+            in_stream++;
+        }
     }
+
+    // If no number has been found, throw the appropriate exception
+    if (in_stream == 0) {
+        throw NoNumberException(to_parse, mode);
+    }
+
+    // Parse the actual number from left to right
+    unsigned int result;
+    std::string num_to_parse = sstr.str();
+    unsigned long times = 1;
+    for (int i = 0; i < num_to_parse.length(); i++) {
+        char c = num_to_parse[num_to_parse.length() - 1 - i];
+
+        // First, check if the value of this number overflows
+        if (((unsigned long)(c - '0')) > ULONG_MAX / times) {
+            throw OverflowException(num_to_parse, to_parse, "uint", mode);
+        }
+
+        // Compute the value of this number
+        unsigned long value = ((unsigned long)(c - '0')) * times;
+
+        // Check if an overflow for result would occur
+        if (result > UINT32_MAX - value) {
+            throw OverflowException(num_to_parse, to_parse, "uint", mode);
+        }
+
+        // Finally, add the value to the result
+        result += value;
+
+        // Update times
+        times *= 10;
+    }
+    return result;
 }
-int StringConverter::toInt(std::string to_parse) {
+int StringConverter::toInt(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
 
-unsigned long StringConverter::toULong(std::string to_parse) {
+unsigned long StringConverter::toULong(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
-long StringConverter::toLong(std::string to_parse) {
+long StringConverter::toLong(std::string to_parse, StringConverterMode mode = NoForgiveness) {
     
 }
--- src/support/StringConverter.h
+++ src/support/StringConverter.h
@@ -11,34 +11,82 @@
 #include <exception>
 
 namespace StringConverter {
+    /* The flexibility of the parser. The first mode means that it exceptions on anything else than the ten decimals, the mode 'AllowSpaces' also allows for spaces, while the other two modes discard all non-numerical characters until a number is found, parse that number and return it. This is the most flexible mode. */
+    enum StringConverterMode {
+        NoForgiveness,
+        AllowSpaces,
+        ParseFirstNumber,
+        ParseLastNumber
+    };
+    /* Converts a StringConverterMode enum to their text representative */
+    static std::string StringConverterModeToText[] = {
+        "NoForgiveness",
+        "AllowSpaces",
+        "ParseFirstNumber",
+        "ParseLastNumber"
+    };
+
     class StringConvertionException : std::exception {
         public:
             const std::string str;
             const std::string msg;
+            const StringConverterMode mode;
+            const std::string mode_name;
 
             /* StringConvertion Base error */
-            StringConvertionException(const std::string to_parse, const std::string message)
+            StringConvertionException(const std::string to_parse, const std::string message, const StringConverterMode forgiveness_mode)
                 : str(to_parse),
-                msg(message)
+                msg(message),
+                mode(forgiveness_mode),
+                mode_name(StringConverterModeToText[forgiveness_mode])
+            {}
+    };
+    class IllegalCharacterException : StringConvertionException {
+        public:
+            const char chr;
+
+            /* Exception for when the mode is too strict and an illegal character has been found. */
+            IllegalCharacterException(const char character, const std::string to_parse, const StringConverterMode forgiveness_mode)
+                : StringConvertionException(to_parse, "Illegal character encountered in: \"" + to_parse + "\": '" + character + "' (mode: " + StringConverterModeToText[forgiveness_mode] + ")", forgiveness_mode),
+                chr(character)
+            {}
+    };
+    class NoNumberException : StringConvertionException {
+        public:
+            /* Exception for when no numeric character has been found in the string (either empty string or when mode is ParseFirstNumber or ParseLastNumber) */
+            NoNumberException(const std::string to_parse, const StringConverterMode forgiveness_mode)
+                : StringConvertionException(to_parse, "No numbers found in: \"" + to_parse + "\"", forgiveness_mode)
+            {}
+    };
+    class OverflowException : StringConvertionException {
+        public:
+            const std::string num;
+            const std::string type;
+        
+            /* Exception for when the to-be-parsed number causes an overflow. */
+            OverflowException(const std::string num_to_parse, const std::string to_parse, const std::string type_name, const StringConverterMode forgiveness_mode)
+                : StringConvertionException(to_parse, "The number \"" + num_to_parse + "\" is too large to be parsed as " + type_name, forgiveness_mode),
+                num(num_to_parse),
+                type(type_name)
             {}
     };
 
     /* Converts the value in a string to an unsigned char. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    unsigned char toUChar(std::string to_parse);
+    unsigned char toUChar(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to a signed char. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    char toChar(std::string to_parse);
+    char toChar(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to an unsigned short. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    unsigned short toUShort(std::string to_parse);
+    unsigned short toUShort(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to a signed short. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    short toShort(std::string to_parse);
+    short toShort(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to an unsigned int. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    unsigned int toUInt(std::string to_parse);
+    unsigned int toUInt(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to a signed int. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    int toInt(std::string to_parse);
+    int toInt(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to an unsigned long. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    unsigned long toULong(std::string to_parse);
+    unsigned long toULong(std::string to_parse, StringConverterMode mode = NoForgiveness);
     /* Converts the value in a string to a signed long. If it fails, an appropriate derivation of StringConvertionException is thrown. */
-    long toLong(std::string to_parse);
+    long toLong(std::string to_parse, StringConverterMode mode = NoForgiveness);
 }
 
 #endif
