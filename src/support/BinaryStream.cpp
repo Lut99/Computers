abee7035cbad19541075ad59f7e0da5e4a314b19
Lut99 <mullertim99@gmail.com>
2019-12-21 18:36:14 +0100

    Added basic functions to BinaryStream. Currently, working on defaulting the size of the internal array.

--- src/support/BinaryStream.cpp
+++ src/support/BinaryStream.cpp
@@ -9,4 +9,80 @@
 *  character array to the internal one.
 **/
 
+#include <limits.h>
+
 #include "BinaryStream.h"
+
+using namespace std;
+using namespace DataTypes;
+
+/* BINARY STRINGS */
+BinaryString::BinaryString(char *data_string, int data_size) {
+    // Simply set these as values
+    this->data = data_string;
+    this->size = data_size;
+}
+BinaryString::BinaryString(char *null_terminated_string) {
+    // Set the values
+    this->data = null_terminated_string;
+    this->size = 0;
+    // Now loop and find the size
+    for (; this->size < INT_MAX; this->size++) {
+        // Stop if the '\0'is found
+        if (null_terminated_string[this->size] == '\0') {
+            break;
+        }
+    }
+    if (this->size == INT_MAX) {
+        // Not found, so set to NULL and size to -1
+        this->data = NULL;
+        this->size = -1;
+    } else {
+        // Add an additional size to account for the '\0' itself
+        this->size++;
+    }
+}
+
+/* BINARY STREAM */
+BinaryStream::BinaryStream(std::size_t initial_size=256) {
+    this->size = 0;
+    this->binary_string = new char[this->size];
+}
+BinaryStream::~BinaryStream() {
+    delete[] this->binary_string;
+}
+
+const std::size_t BinaryStream::read(char *result) const {
+    // Copy this->size elements to the other array and return the amount copied
+    for (std::size_t i = 0; i < this->size; i++) {
+        result[i] = this->binary_string[i];
+    }
+    return this->size;
+}
+const std::size_t BinaryStream::flush(char *result) {
+    // Copy this->size elements to the other array and return the amount copied
+    for (std::size_t i = 0; i < this->size; i++) {
+        result[i] = this->binary_string[i];
+    }
+    std::size_t to_return = this->size;
+
+    // Before returning, reset the buffer
+    this->clear();
+
+    return to_return;
+}
+
+const std::size_t BinaryStream::length() const {
+    return this->size;
+}
+
+void BinaryStream::clear() {
+    delete[] this->binary_string;
+    this->size = 0;
+    this->binary_string = new char[this->size];
+}
+
+/* STREAM OPERATORS */
+BinaryStream& operator<<(BinaryStream& bs, const unsigned char& value) {
+
+}
--- src/support/BinaryStream.h
+++ src/support/BinaryStream.h
@@ -7,10 +7,60 @@
 #ifndef BINARYSTREAM_H
 #define BINARYSTREAM_H
 
-namespace std {
-    class BinaryStream {
-        
+#include <cstddef>
+
+namespace DataTypes {
+    struct BinaryString {
+        char *data;
+        int size;
+
+        /* Create a BinaryString object by ptr and size. Note: does nothing about allocation or deallocation. */
+        BinaryString(char *data_string, int data_size);
+
+        /* Create a BinaryString object by providing a null-terminated char*. Note: does nothing about allocation or deallocation. */
+        BinaryString(char *null_terminated_string);
     };
+
+    class BinaryStream {
+        private:
+            char *binary_string;
+            std::size_t size;
+            std::size_t real_size;
+
+            /* Appends n characters to the internal array, which is resized automatically. To be dynamic, the actual size is often higher than the contents. */
+
+        public:
+            /* The BinaryStream class makes constructing BinaryStreams easier. Simply use the '<<' operator to add various types of data. You can also define such an operator yourself by using '<< BinaryString' to add binary data yourself. */
+            BinaryStream(std::size_t initial_size=256);
+            ~BinaryStream();
+            
+            /* Returns a copy of the contents of the BinaryStream. The size of this data is returned, and the data itself copied to given pointer. */
+            const std::size_t read(char *result) const;
+            /* Flushes the contents of the BinaryStream. The size of this data is returned, and the data itself copied to given pointer. */
+            const std::size_t flush(char *result);
+
+            /* Returns the length of the stream right now. */
+            const std::size_t length() const;
+
+            /* Clears the internal buffer and resets the stream to have no content. */
+            void clear();
+
+            /* Declares the necessary << operators as friends. */
+            /* Appends unsigned chars to the stream */
+            friend BinaryStream& operator<<(BinaryStream& bs, const unsigned char& value);
+            /* Appends chars to the stream */
+            /* Appends unsigned shorts to the stream */
+            /* Appends shorts to the stream */
+            /* Appends unsigned integers to the stream */
+            /* Appends integers to the stream */
+            /* Appends unsigned longs to the stream */
+            /* Appends longs to the stream */
+            /* Appends strings to the stream */
+            /* Appends BinaryStrings to the stream */
+            /* Allows the binarystream to be appended to an ostream */
+    };
+
+    BinaryStream& operator<<(BinaryStream& bs, const unsigned char& value);
 }
 
 #endif
