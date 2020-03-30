32ef77da43a598f14f557b83516c731d52727a05
Lut99 <mullertim99@gmail.com>
2020-03-30 20:14:26 +0200

    Started again with making the hardware side to know better how to handle the software side.

---
+++ src/hardware/src/Computer.cpp
@@ -0,0 +1,24 @@
+/* COMPUTER.cpp
+ *   by Lut99
+ *
+ * Created:
+ *   3/30/2020, 8:08:15 PM
+ * Last edited:
+ *   3/30/2020, 8:14:23 PM
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   The computer class is the basis of any computer, as the name suggests.
+ *   Mainly, it handles the most basic form of bootup and shutdown of the
+ *   computer, and it handles the coorporation between threads that serve
+ *   the various computer components.
+**/
+
+#include "Computer.hpp"
+
+using namespace std;
+using namespace Computer;
+
+
+
